886e52bf74ce1d92c201aaa94787a653a16060bf
DianaGru <dianchik2004@gmail.com>
2022-06-22 19:06:06 +0300

    Right HomeWork

--- HomeWork2/src/test/java/FromCssToXPath.java
+++ HomeWork2/src/test/java/FromCssToXPath.java
@@ -14,27 +14,36 @@ public class FromCssToXPath {
 
         wd.findElement(By.cssSelector("a[href]"));
         wd.findElement(By.xpath("//*[@a ='href']"));
+        wd.findElement(By.xpath("//a[@href]"));//right
 
         wd.findElement(By.cssSelector("[href ^='/ho']"));
         wd.findElement(By.xpath("//*[starts-with = @href, '/ho']"));
+        wd.findElement(By.xpath("//*[starts-with(@href, '/ho')]"));//right
 
         wd.findElement(By.cssSelector("div.focus"));
         wd.findElement(By.xpath("//*div[@class = 'focus']]"));
+        wd.findElement(By.xpath("//div[@class = 'focus']"));//right
+        wd.findElement(By.xpath("//div[contains(@class, 'focus')]"));//right
 
         wd.findElement(By.cssSelector("input[type='text']"));
         wd.findElement(By.xpath("//*input[@type = 'text']"));
+        wd.findElement(By.xpath("//input[@type = 'text']"));//right
 
         wd.findElement(By.cssSelector("div #host"));
         wd.findElement(By.xpath("//*div[@id='host']"));
+        wd.findElement(By.xpath("//div//*[@id='host']")); //right
 
         wd.findElement(By.cssSelector("table#contry tr:nt-child(3)) td:last-child"));
-        wd.findElement(By.xpath("//*table[@id = 'contry']//tr[3]//td[last]"));
+        wd.findElement(By.xpath("//*table[@id = 'country']//tr[3]//td[last]"));
+        wd.findElement(By.xpath("//table[@id='country']//tr[3]//td[last()]")); //right
 
         wd.findElement(By.cssSelector("div#idName li:first-child"));
-        wd.findElement(By.xpath("//*div[@id = 'idName']//li[last[1]"));
+        wd.findElement(By.xpath("//*div[@id = 'idName']//li[last[1]]"));
+        wd.findElement(By.xpath("//div[@id = 'idName']//li[1]"));//right
 
         wd.findElement(By.cssSelector("div#sort>div:nth-child(2)"));
         wd.findElement(By.xpath("//*div[@id = 'sort']/div[2]"));
+        wd.findElement(By.xpath("//div[@id = 'sort']/div[2]"));//right
 
     }
 }
--- HomeWork2/src/test/java/FromXPathToCss.java
+++ HomeWork2/src/test/java/FromXPathToCss.java
@@ -13,27 +13,35 @@ public class FromXPathToCss {
 
         wd.findElement(By.xpath("//*[@id='host"));
         wd.findElement(By.cssSelector("#'host'"));
+        wd.findElement(By.cssSelector("#host"));//right
 
         wd.findElement(By.xpath("//button[@class='submit']"));
         wd.findElement(By.cssSelector("button.submit"));
+        wd.findElement(By.cssSelector("button.submit"));//right
 
         wd.findElement(By.xpath("//div/div/img[@src='paris.jpg']"));
         wd.findElement(By.cssSelector("div>div>img[src='paris.jpg']"));
+        wd.findElement(By.cssSelector("div>div>img[src='paris.jpg']"));//right
 
         wd.findElement(By.xpath("//div[@class='first' and @class='list']"));
         wd.findElement(By.cssSelector("div.first > .list"));
+        wd.findElement(By.cssSelector("div.first.list"));//right
 
         wd.findElement(By.xpath("//div[contains(@class,'st']"));
         wd.findElement(By.cssSelector("div[class$='st']"));
+        wd.findElement(By.cssSelector("div[class *= 'st']"));//right
 
         wd.findElement(By.xpath("//div[@id=’idName’]//h1[last()]"));
         wd.findElement(By.cssSelector("div#'idName' h1:last-child"));
+        wd.findElement(By.cssSelector("div#idName h1:last-child"));//right
 
         wd.findElement(By.xpath("//table[@id]//tr[last()]"));
         wd.findElement(By.cssSelector("table# tr:last-child"));
+        wd.findElement(By.cssSelector("table[id] tr:last-child"));//right
 
         wd.findElement(By.xpath("//a[starts-with(@id,'ret')]"));
         wd.findElement(By.cssSelector("a=[id^ = 'ret']"));
+        wd.findElement(By.cssSelector("a[id ^= 'ret']"));//right
 
     }
 }
